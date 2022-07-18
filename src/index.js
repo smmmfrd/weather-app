13d36d862ad44d338f58c4d02ee0e86b04277e2f
Sam Mumford <smmmfrd@gmail.com>
2022-07-18 15:47:56 -0700

    API Call Functionality RIP Key

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './index.css';
 
-console.log('here');
+async function getWeather(){
+    var response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4cdf9b1699a7b1a192527fe2c3641e33');
+    var data = await response.json();
+    console.log(data);
+}
+
+getWeather().catch(err => {
+    console.log(err);
+});
